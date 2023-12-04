b7bb92a6714916f75b8187d8da8a125ea91b5aba
seowon <sonseowon5@gmail.com>
2023-12-05 00:59:57 +0900

    using queue

--- Q_1158_2.java
+++ Q_1158_2.java
@@ -3,10 +3,7 @@ package coding_test_practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q_1158_2 {
     public static void main(String[] args) throws IOException {
@@ -14,9 +11,14 @@ public class Q_1158_2 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        Character[] ch = new Character[2];
 
         Queue<Integer> queue = new LinkedList<>();
         ArrayList<String> result = new ArrayList<>();
+        String[] s = new String[5];
+        result.add("aaaa");
+        System.out.println(result);
+
         for (int i = 1; i <= N; i++) {
             queue.add(i);
         }
