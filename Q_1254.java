f7d78096e82db739de0b80b2fb32f513706c155a
seowon <sonseowon5@gmail.com>
2023-12-05 01:54:48 +0900

    palindrome - fix with index comparation

--- Q_1254.java
+++ Q_1254.java
@@ -3,38 +3,34 @@ package coding_test_practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+// idea : sunstring을 이용해, 해당 인덱스 기준 그 이후 모든 글자가 palindrome인지 판별
 
 public class Q_1254 {
     public static void main(String[] args) throws IOException{
-        int answer = 0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
 
         if(S == null || S.isEmpty()) return;
 
-
-        Deque<Character> deq = new ArrayDeque<>();
-        for(char c : S.toCharArray()) deq.addLast(c);
-
-        ArrayList<Character> result = new ArrayList<>();
-        while (deq.size() > 1) {
-            char a = deq.pollFirst();
-            char b = deq.pollLast();
-
-            if(a == b) {
-                result.add(a);
-                result.add(b);
-            } else {
-                result.add(a);
-                result.add(a);
-                deq.addLast(b);
+        int idx = S.length() - 1;
+        for (int i = 0; i < idx; i++) {
+            if(isPalindrome(S.substring(i))) {
+                idx = i;
+                break;
             }
         }
-        answer = deq.isEmpty() ? result.size() : result.size() + deq.size();
-        System.out.println(answer);
+        System.out.println(S.length() + idx);
+    }
+
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 }
