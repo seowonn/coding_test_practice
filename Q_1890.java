db9a10a354b37ab93c41de9503317fbc2e852766
seowon <sonseowon5@gmail.com>
2023-12-06 01:30:48 +0900

    fix - dynamic programming

--- Q_1890.java
+++ Q_1890.java
@@ -1,43 +1,33 @@
 package coding_test_practice;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Q_1890 {
-    static int[][] dirs = {{0, 1}, {1, 0}};
     public static void main(String[] args) throws IOException {
         StringTokenizer st;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        // 경로 개수 조건에 의해 long 타입으로 설정. 2^63 - 1
+        long[][] dp = new long[N+1][N+1];
 
-        int[][] board = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        dp[1][1] = 1;
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= N; j++) {
+                int moveLength = Integer.parseInt(st.nextToken());
+
+                if(dp[i][j] >= 1 && moveLength != 0) {
+                    // 이동 가능한 경로마다 1씩 더해주게 되면, 최종 목적지에는 도달 가능한 경우의 수가 나오게 된다.
+                    if(j + moveLength <= N) dp[i][j + moveLength] += dp[i][j];
+                    if(i + moveLength <= N) dp[i + moveLength][j] += dp[i][j];
+                }
             }
         }
-        int cnt = 0;
-        cnt += dfs(board, 0, 0, 0);
-        System.out.println(cnt);
-    }
-
-    public static int dfs(int[][] board, int i, int j, int cnt) {
-        int move = board[i][j];
-        for(int[] dir : dirs) {
-            int xNext = i + dir[0] * move;
-            int yNext = j + dir[1] * move;
-
-            if(xNext < 0 || xNext >= board.length || yNext < 0 || yNext >= board.length) return 0;
-            if(board[xNext][yNext] == 0) {
-                cnt++;
-                return cnt;
-            }
-            cnt += dfs(board, xNext, yNext, 0);
-        }
-        return cnt;
+        bw.write(dp[N][N] + "\n");
+        bw.flush();
     }
 }
