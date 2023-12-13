453f335a7c48ffa4d319c3490fbdff73231a4b01
seowon <sonseowon5@gmail.com>
2023-12-13 16:44:01 +0900

    divide&conquer - find pattern!

---
+++ divide_and_conquer/Q_2630.java
@@ -0,0 +1,58 @@
+package coding_test_practice.divide_and_conquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_2630 {
+    static int[][] colorPaper;
+    static int white = 0;
+    static int blue = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        colorPaper = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                colorPaper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dividePaper(0, 0, N);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    public static void dividePaper(int startX, int startY, int n) {
+        if(colorCheck(startX, startY, n)) {
+            if(colorPaper[startX][startY] == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+            return;
+        }
+
+        dividePaper(startX, startY, n / 2);
+        dividePaper(startX + n / 2, startY,n / 2);
+        dividePaper(startX, startY + n / 2, n / 2);
+        dividePaper(startX + n / 2, startY + n / 2,  n / 2);
+    }
+
+    // 분할된 종이의 색깔이 같은지 검사한다.
+    public static boolean colorCheck(int startX, int startY, int n) {
+        int color = colorPaper[startX][startY];
+
+        for (int i = startX; i < startX + n; i++) {
+            for (int j = startY; j < startY + n; j++) {
+                if(colorPaper[i][j] != color) return false;
+            }
+        }
+        return true;
+    }
+}
