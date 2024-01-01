9425672bdc4e064b6385c3d3b8622881cfe39853
seowon <sonseowon5@gmail.com>
2024-01-02 00:22:21 +0900

    calculate the cumulative sum - try to use dp

---
+++ dynamic_programming/Q_11659.java
@@ -0,0 +1,30 @@
+package coding_test_practice.dynamic_programming;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] totalDp = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++)
+            totalDp[i] = totalDp[i - 1] + Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p1 = Integer.parseInt(st.nextToken());
+            int p2 = Integer.parseInt(st.nextToken());
+            bw.write(totalDp[p2] - totalDp[p1 - 1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
